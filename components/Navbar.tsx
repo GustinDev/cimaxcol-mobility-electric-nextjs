20dc747424a5c434f1778757ed534843e548fe58
Juan Gustin <juandgustin@gmail.com>
2023-07-04 18:30:59 -0500

    NavBar: V2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,32 +6,39 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <header className='w-full absolute z-10'>
+    <header className='w-full absolute z-10 bg-white'>
       <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'>
         <Link
           className='flex justify center items-center'
           href='/'
         >
           <Image
-            src='/logo.svg'
+            src='/motorbike.svg'
             alt='CMELogo'
-            width={118}
-            height={18}
-            className='object-contain'
+            width={30}
+            height={30}
+            className='object-contain mr-2'
           ></Image>
+
+          <h1 className='font-extrabold text-2xl'>CME</h1>
 
           <CustomButtom
             title='Inicio'
             btnType='button'
-            containerStyles='text-primary-blue rounded-full bg-white min-w-[100px]'
+            containerStyles='text-primary-blue rounded-full  min-w-[100px] hover:font-semibold'
           />
         </Link>
         <div className='flex'>
-          <CustomButtom
-            title='Ingresar'
-            btnType='button'
-            containerStyles='text-primary-blue rounded-full bg-white min-w-[100px]'
-          />
+          <Link
+            className='flex justify center items-center'
+            href='/login'
+          >
+            <CustomButtom
+              title='Ingresar'
+              btnType='button'
+              containerStyles='text-primary-blue rounded-full  min-w-[100px] hover:font-semibold'
+            />
+          </Link>
           <Link
             className='flex justify center items-center'
             href='/contact'
@@ -39,7 +46,7 @@ const Navbar = (props: Props) => {
             <CustomButtom
               title='Contacto'
               btnType='button'
-              containerStyles='text-primary-blue rounded-full bg-white min-w-[100px]'
+              containerStyles='text-primary-blue rounded-full  min-w-[100px] hover:font-semibold'
             />
           </Link>
           <Link
@@ -49,7 +56,7 @@ const Navbar = (props: Props) => {
             <CustomButtom
               title='Nosotros'
               btnType='button'
-              containerStyles='text-primary-blue rounded-full bg-white min-w-[100px]'
+              containerStyles='text-primary-blue rounded-full bg-white min-w-[100px] hover:font-semibold'
             />
           </Link>
         </div>
