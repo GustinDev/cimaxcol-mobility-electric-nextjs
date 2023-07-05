c3410bf65279aa764bd5124c142060e0123d17df
Juan Gustin <juandgustin@gmail.com>
2023-07-05 18:19:23 -0500

    Contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Contact = (props: Props) => {
   return (
     <div className='flex-1 pt-28 padding-x'>
-      <h1>Contactanos</h1>
+      <h1>Contactanos: V2</h1>
     </div>
   );
 };
